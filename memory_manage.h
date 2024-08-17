b3705689eb1b7f39a5b011c9cf86e4282642646c
asem905 <123882184+asem905@users.noreply.github.com>
2024-08-17 16:55:04 +0300

    Update memory_manage.h

--- memory_manage.h
+++ memory_manage.h
@@ -6,7 +6,7 @@ comment:contains all functions declarartions used in memory_manage.c and app.c
 
 #ifndef MEMORY_MANAGE_H
 #define MEMORY_MANAGE_H
-/*-----------------includes section--------------*/
+/-----------------includes section--------------/
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
@@ -22,20 +22,23 @@ comment:contains all functions declarartions used in memory_manage.c and app.c
 #include <sys/sysinfo.h>
 #include <time.h>
 #include <aio.h>
-/*----------------macros declarations-----------------*/
-#define HEAP_SIZE 		1024 * 1024 *1024 
+#include <error.h>
+/----------------macros declarations-----------------/
+#define HEAP_SIZE 		1024 * 1024 * 1024 
 #define ALLOCATED_AREA		1
 #define UNALLOCATED_AREA	0
-#define DEFAULT_ADDED_SIZE	128 * 1024
-#define DEFAULT_SUBTRACTED_SIZE	122 * 1024
-/*-------------------data types------------------*/
+#define DEFAULT_ADDED_SIZE	128 * 1024 
+#define DEFAULT_SUBTRACTED_SIZE	122 * 1024 
+#define MAX_ALLOC_SIZE 		(10*DEFAULT_ADDED_SIZE / 10)
+#define START_SIZE		1024
+/-------------------data types------------------/
 typedef struct blockheader{
 	size_t block_size;
 	char allocation_status;
 	struct blockheader *next_block;
 	struct blockheader *prev_block;
 }blockheader_t;
-/*-----------section contains functions declarations used inshell.c file------------*/
+/-----------section contains functions declarations used inshell.c file------------/
 void *HmmAlloc(size_t size);
 void HmmFree(void *ptr);
 #endif
