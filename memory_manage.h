9b75ff6e3311a65f1529a17d2b50f08dad9bfb6f
Assem Samy Mohsen <123882184+asem905@users.noreply.github.com>
2024-09-11 17:47:41 +0300

    Update memory_manage.h

--- memory_manage.h
+++ memory_manage.h
@@ -32,16 +32,13 @@ comment:contains all functions declarartions used in memory_manage.c and app.c
 #define MAX_ALLOC_SIZE 				(10*DEFAULT_ADDED_SIZE / 10)
 #define START_SIZE				1024
 
-#define ALLOCATE_USING_MY_MALL0C 		1
-#define ALLOCATE_USING_SBRK			0
-#define METHOD_OF_ALLOCATION			ALLOCATE_USING_MY_MALL0C 
 
-#define FREEING_USING_MY_FREE 			1
-#define FREEING_USING_SBRK			0
-#define METHOD_OF_FREEING			FREEING_USING_MY_FREE 
+#define FREEING_ALLOCATING_USING_MY_FREE_MALLOC 1
+#define FREEING_ALLOCATINF_USING_SBRK		0
+#define METHOD_OF_FREEING_ALLOCATING		FREEING_ALLOCATING_USING_SBRK
 
 #define ALIGN_SIZE 8
-#define align_size(size) (((size) + (ALIGN_SIZE - 1)) & ~(ALIGN_SIZE - 1))
+#define align_size(size) (((size) + (ALIGN_SIZE - 1)) & ~(ALIGN_SIZE - 1)) //just to allign to 8 bytes size to ease access of it in memry
 
 /-------------------data types------------------/
 typedef struct blockheader{
@@ -54,7 +51,7 @@ typedef struct blockheader{
 /-----------section contains functions declarations used inshell.c file------------/
 
 void *realloc(void *ptr,size_t size);
-void *calloc(size_t nmemb, size_t size);
+void* calloc(size_t nmemb, size_t size);
 void *malloc(size_t size);
 void free(void *ptr) ;
 #endif
